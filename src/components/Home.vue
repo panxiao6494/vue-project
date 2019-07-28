e6672c5eb4b312437b548e781704a9dd98b296ff
萧岳 <1592961517@qq.com>
2019-07-28 19:53:34 +0800

    vue实战提交1

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template xmlns="b">
+  <div>
+    <el-row>
+      <el-button type="info">退出</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
